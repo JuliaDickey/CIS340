c5c6a298741f1753c54226b9ddd12d6eaa4108aa
JuliaDickey <103400555+JuliaDickey@users.noreply.github.com>
2022-05-09 08:08:07 +0000

    MyNinthProject

--- MyEighthProject/App.js
+++ MyEighthProject/App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from
+'react';
+import { StyleSheet, Text, View, TouchableOpacity } from
+'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+  const [selectedImage, setSelectedimage ] =React.useState(null)
+
+  let openImagepickerAsync = async () => {
+  let permissionResult =await ImagePicker.requestCameralRollPermissionssync();
+  if (permissionResult.granted=== false){
+  alert ("Permisison is required!");
+  return;
+  }
+  let pickerResult =await ImagePicker.launchImageLibraryAsync();
+
+  if (pickerResult.cancelled === true) {
+  return;
+  }
+  setSelectedImage({ localUri: pickerResult.uri});
+  
+  // display the selected image
+  if (selectedImage !== null) {
+  return(
+  <View style = {styles.container}>
+  <Image source={{ uri: selectedImage.localUri}} style={styles.selImage}/>
+  </View>
+  )
 }
+ // end of the code
+
+  return (
+  <View style={styles.container}>
+  <Image source={{uri:
+  'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/logo.png'}}
+  style={styles.log0} />
+
+  <Text style={styles.insts}>
+  Press the button below to select an image on your phone!
+  </Text>
+  <TouchableOpacity style={styles.button} onPress={() => alert ('You have not selected an image yet')}>
+  <Text style={styles.buttonText}>Pick Image</Text>
+  </TouchableOpacity>
+  </View>
+  );
+
+  
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+container: {
+flex: 1,
+justifyContent: 'center',
+paddingHorizontal:20,
+backgroundColor: "#FFFFE0",
+},
+button: {
+alignItens: "center",
+backgroundcolor:"#778899",
+padding: 10
   },
-});
+
+  }
+)}};
